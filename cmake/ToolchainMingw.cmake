5a591939eb51bc23a227652d472893d32fd1aee4
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-06-25 14:28:46 +0100

    Clean Mingw cross compile toolchain

--- cmake/ToolchainMingw.cmake
+++ cmake/ToolchainMingw.cmake
@@ -5,15 +5,13 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
 
-
-
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32 /home/alex/mingw-install )
+set(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
