416eb477415b0cc9bdd09dad27088ced1d303c8e
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-06-25 19:46:25 +0100

    Add support for the PDCurses curses library
    - Public Domain Curses is a curses compatible library that runs
      on windows

---
+++ cmake/FindPDCurses.cmake
@@ -0,0 +1,27 @@
+# - Try to find PDCurses
+# Once done, this will define
+#
+#  PDCurses_FOUND        - system has PDCurses
+#  PDCurses_INCLUDE_DIRS - the PDCurses include directories
+#  PDCurses_LIBRARIES    - link these to use PDCurses
+
+include(CheckCSourceCompiles)
+include(LibFindMacros)
+
+find_path(PDCurses_INCLUDE_DIR
+    NAMES curses.h term.h
+)
+
+find_library(PDCurses_LIBRARY
+    NAMES pdcurses libpdcurses.a
+)
+
+if (PDCurses_INCLUDE_DIR)
+  set(CMAKE_REQUIRED_INCLUDES "${PDCurses_INCLUDE_DIR}")
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PDCurses DEFAULT_MSG
+		PDCurses_LIBRARY PDCurses_INCLUDE_DIR)
+
+
