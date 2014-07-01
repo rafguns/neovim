4c3d512994a710abb549d1df6f0566fe5244def5
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-07-01 20:54:08 +0100

    Define uid_t for windows

--- src/nvim/os/win_defs.h
+++ src/nvim/os/win_defs.h
@@ -2,8 +2,11 @@
 #define NEOVIM_OS_WIN_DEFS_H
 
 #include <windows.h>
+#include <uv.h>
 
 #define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
 #define TEMP_FILE_PATH_MAXLEN _MAX_PATH
+
+typedef uv_uid_t uid_t;
 
 #endif  // NEOVIM_OS_WIN_DEFS_H
