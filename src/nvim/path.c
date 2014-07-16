f67d6a49cc8d3f6c15206b3c14ce2e3671b0594f
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-07-16 19:40:04 +0100

    FIXME: No VLA in MSVC

--- src/nvim/path.c
+++ src/nvim/path.c
@@ -1988,7 +1988,8 @@ static int path_get_absolute_path(char_u *fname, char_u *buf, int len, int force
   char_u *p;
   *buf = NUL;
 
-  char relative_directory[len];
+  // FIXME(equalsraf): refactor this out of here
+  char *relative_directory = xmalloc(len);
   char *end_of_path = (char *) fname;
 
   // expand it if forced or not an absolute path
@@ -2003,9 +2004,11 @@ static int path_get_absolute_path(char_u *fname, char_u *buf, int len, int force
     }
 
     if (FAIL == path_full_dir_name(relative_directory, (char *) buf, len)) {
+      free(relative_directory);
       return FAIL;
     }
   }
+  free(relative_directory);
   return append_path((char *) buf, (char *) end_of_path, len);
 }
 
