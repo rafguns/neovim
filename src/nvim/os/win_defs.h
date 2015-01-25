cc5d1b26144657d812e76b99310a7814bc3146da
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-01-25 02:18:42 +0000

    Define SSIZE_MAX for Windows

--- src/nvim/os/win_defs.h
+++ src/nvim/os/win_defs.h
@@ -13,6 +13,10 @@
 #include <time.h>
 #include <sys/stat.h>
 
+#include <uv.h>
+
+#include "auto/config.h"
+
 // For MSVC
 #ifdef MSVC
 # ifndef restrict
@@ -54,7 +58,16 @@
 # define read _read
 # define write _write
 # define snprintf _snprintf
-#endif
+
+# ifndef SSIZE_MAX
+#  if defined(_WIN64)
+#   define SSIZE_MAX _I64_MAX
+#  else
+#   define SSIZE_MAX LONG_MAX
+#  endif
+# endif
+
+#endif // MSVC
 
 #define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
 #define TEMP_FILE_PATH_MAXLEN _MAX_PATH
