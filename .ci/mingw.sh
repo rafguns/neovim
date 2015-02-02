0a1362fff63cf8a7f1abff0a3676d469f0c57379
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-02-02 19:14:11 +0000

    Travis Mingw

---
+++ .ci/mingw.sh
@@ -0,0 +1,18 @@
+. "$CI_SCRIPTS/common.sh"
+
+sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 wine
+
+# Build third-party deps
+mkdir .deps
+cd .deps
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw32-w64-cross-travis.toolchain.cmake ../third-party/
+cmake --build . -- VERBOSE=1
+cd ..
+
+# Build Neovim
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/mingw32-w64-cross-travis.toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="-DMIN_LOG_LEVEL=0 -pg" .. 
+cmake --build . -- VERBOSE=1
+wine bin/nvim.exe --version
+
