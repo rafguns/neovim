a794ff76e7046c79b8dd29100361e3e8cf20f9df
Rui Abreu Ferreira <raf-ep@gmx.com>
2015-02-03 10:46:44 +0000

    bump

--- .ci/mingw.sh
+++ .ci/mingw.sh
@@ -4,8 +4,8 @@
 #sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 mingw-w64-dev mingw-w64-tools wine 
 
 # mingw-w64 build from http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/gcc-4.8-release/
-wget "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/gcc-4.8-release/i686-w64-mingw32-gcc-4.8.0-linux64_rubenvb.tar.xz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FToolchains%2520targetting%2520Win32%2FPersonal%2520Builds%2Frubenvb%2Fgcc-4.8-release%2F&ts=1422959985&use_mirror=heanet"
-sudo tar -axf i686-w64-mingw32-gcc-4.8.0-linux64_rubenvb.tar.xz -C /opt
+wget "http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Personal%20Builds/rubenvb/gcc-4.8-release/i686-w64-mingw32-gcc-4.8.0-linux64_rubenvb.tar.xz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FToolchains%2520targetting%2520Win32%2FPersonal%2520Builds%2Frubenvb%2Fgcc-4.8-release%2F&ts=1422959985&use_mirror=heanet" -O mingw.tar.xz
+sudo tar -axf mingw.tar.xz -C /opt
 
 i686-w64-mingw32-gcc --version
 mkdir .deps
